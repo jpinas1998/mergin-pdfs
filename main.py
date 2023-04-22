40b445b6e7f6f37fc57f1f9eac87fb1ce610c669
Joaquín Pina Socorro <66146829+jpinas1998@users.noreply.github.com>
2023-04-22 12:49:38 -0400

    feat: sort files

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import glob
-from PyPDF2 import PdfFileMerger, PdfFileReader
+from PyPDF2 import PdfMerger, PdfReader
 
-mergedObject = PdfFileMerger()
+mergedObject = PdfMerger()
 
 # list all pdf files in current directory
 files = glob.glob("*.pdf")
 
 if files:
     print(f"Detected the following files: {files}")
-
+    files.sort()
     for file in files:
-        mergedObject.append(PdfFileReader(file, 'rb'))
+        mergedObject.append(PdfReader(file, 'rb'))
 
     print(f"Joining all files into one...")
     
-    mergedObject.write("merged-pdf.pdf")
+    mergedObject.write("pdf-mama.pdf")
 
     print("Done!")
 
